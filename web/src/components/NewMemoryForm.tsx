38e39818e950317c9567776b8730119573d56aeb
Paulo Santana <paulosantana.sa@gmail.com>
2023-06-19 02:13:53 -0300

    Continuidade da Aula 05

--- web/src/components/NewMemoryForm.tsx
+++ web/src/components/NewMemoryForm.tsx
@@ -3,14 +3,48 @@
 import { Camera } from 'lucide-react'
 import { MediaPicker } from './MediaPicker'
 import { FormEvent } from 'react'
+import Cookie from 'js-cookie'
+import { api } from '@/lib/api'
+import { useRouter } from 'next/navigation'
 
 export function NewMemoryForm() {
-  function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
+  const router = useRouter()
+
+  async function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
     const formData = new FormData(event.currentTarget)
 
-    console.log(Array.from(formData.entries()))
+    const fileToUpload = formData.get('coverUrl')
+
+    let coverUrl = ''
+
+    if (fileToUpload) {
+      const uploadFormData = new FormData()
+      uploadFormData.set('file', fileToUpload)
+
+      const uploadResponse = await api.post('/upload', uploadFormData)
+
+      coverUrl = uploadResponse.data
+    }
+
+    const token = Cookie.get('token')
+
+    await api.post(
+      '/memories',
+      {
+        coverUrl,
+        content: formData.get('content'),
+        isPublic: formData.get('isPublic'),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    router.push('/')
   }
 
   return (
